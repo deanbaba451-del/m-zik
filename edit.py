7faf00bca8435af4ced9f7bc00bc3ffd2823a880
deanbaba451-del <deanbaba451@gmail.com>
2026-03-14 17:20:01 +0300

    Create edit.py

---
+++ edit.py
@@ -0,0 +1,24 @@
+import telebot
+
+TOKEN = "8690542685:AAGAB5grLaJ78rGay-7R7hh6VGpgKQy_hNo"
+bot = telebot.TeleBot(TOKEN)
+
+@bot.edited_message_handler(func=lambda message: True)
+def lol(message):
+    try:
+        if message.text or message.caption:
+            chat_id = message.chat.id
+            message_id = message.message_id
+            user_id = message.from_user.id
+            user_name = message.from_user.first_name
+
+            bot.delete_message(chat_id, message_id)
+
+            response_text = f"[{user_name}](tg://user?id={user_id}), bu grupta mesaj düzenlemek yasaktır."
+            bot.send_message(chat_id, response_text, parse_mode="Markdown")
+
+    except Exception as e:
+        print(f"{e}")
+
+if __name__ == "__main__":
+    bot.infinity_polling()
